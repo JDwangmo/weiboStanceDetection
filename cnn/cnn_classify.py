6216cce47f5af9a8f832fedd0daaf4f814606daa
JDwang <383287471@qq.com>
2016-05-07 16:20:25 +0800

    修改了cnn_train

--- cnn/cnn_classify.py
+++ cnn/cnn_classify.py
@@ -15,11 +15,14 @@ X_test, y_test = load_data(test_dataA_result_path,
                            )
 X_test = X_test.reshape(X_test.shape[0],1,X_test.shape[1],X_test.shape[2])
 y_test_onehot = np_utils.to_categorical(y_test,3)
+# 多少次迭代
 nb_epoch = 32
+# 多少种类型的卷积核
+num_wins = 5
 model_architecture = '/home/jdwang/PycharmProjects/weiboStanceDetection/cnn/model/' \
-                     'cnn_model_architecture_%depoch.json'%(nb_epoch)
+                     'cnn_model_architecture_%depoch_%dwin.json'%(nb_epoch,num_wins)
 model_weights = '/home/jdwang/PycharmProjects/weiboStanceDetection/cnn/model/' \
-                'cnn_model_weights_%depoch.h5'%(nb_epoch)
+                'cnn_model_weights_%depoch_%dwin.h5'%(nb_epoch,num_wins)
 
 # load
 model = model_from_json(open(model_architecture).read())
@@ -47,7 +50,8 @@ test_data = pd.read_csv('/home/jdwang/PycharmProjects/weiboStanceDetection/train
 test_data['IS_CORRECT'] =  (y_pred==y_test)
 test_data['PREDICT'] =  [idx2target[item] for item in y_pred]
 result_path = '/home/jdwang/PycharmProjects/weiboStanceDetection/cnn/result/' \
-              'cnn_result.csv'
+              'cnn_result_%depoch_%dwin.csv'%(nb_epoch,num_wins)
+
 test_data[[u'﻿ID','TARGET','TEXT','STANCE','IS_CORRECT','PREDICT']].to_csv(
     result_path,
     sep='\t',
--- cnn/cnn_train.py
+++ cnn/cnn_train.py
@@ -2,8 +2,15 @@
 
 from data_processing.load_data import load_data
 from keras.models import Sequential
-from keras.layers import Convolution2D,MaxPooling2D,Activation,Flatten,Dense,Dropout
+from keras.layers import Convolution2D,MaxPooling2D,Activation,Flatten,Dense,Dropout,Merge,InputLayer
 from keras.utils import np_utils
+import logging
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
+                    level=logging.DEBUG
+                    )
+
+
 
 dev_dataA_result_path = '/home/jdwang/PycharmProjects/weiboStanceDetection/train_data/' \
                         'dev_data.csv'
@@ -26,23 +33,66 @@ y_test_onehot = np_utils.to_categorical(y_test,3)
 
 # cnn arch
 image_row,image_col = 173,3
+# 创建一个模型
 model = Sequential()
-layers = [
-    Convolution2D(32,5,3,input_shape=(1,image_row,image_col),border_mode='valid'),
-    Activation('relu'),
-    MaxPooling2D(pool_size=(10,1)),
-    Convolution2D(32,5,1),
-    Activation('relu'),
-    MaxPooling2D(pool_size=(2,1)),
-    # Dropout(0.8),
-    Flatten(),
+# 多窗口核卷积层
+cnn_layers = []
+n_wins = [5,10,20,25,30,40,45,50]
+logging.debug('使用%d种卷积核，大小分别为：%s'%(len(n_wins),str(n_wins)))
+for win_size in n_wins:
+
+    m = Sequential()
+    layers = [
+        Convolution2D(32, win_size, 3, input_shape=(1, image_row, image_col), border_mode='valid'),
+        Activation('relu'),
+        MaxPooling2D(pool_size=(10, 1)),
+        Dropout(0.9),
+        Convolution2D(32, 4, 1),
+        Activation('relu'),
+        MaxPooling2D(pool_size=(2, 1)),
+        Dropout(0.5),
+        Flatten(),
+        # Dense(60),
+        # Dropout(0.8),
+    ]
+    [m.add(layer) for layer in layers]
+    cnn_layers.append(m)
+
+
+model.add(Merge(layers=cnn_layers,
+                concat_axis=1,
+                mode='concat'
+                )
+          )
+# 隐含层
+hidden_layers = [
     Dense(60),
-    # Dropout(0.8),
+    Dropout(0.8),
     Activation('relu'),
     Dense(3),
     Activation('softmax')
 ]
-[model.add(layer) for layer in layers]
+[model.add(layer) for layer in hidden_layers]
+
+
+# 单窗口核卷积层
+# layers = [
+#     Convolution2D(32,5,3,input_shape=(1,image_row,image_col),border_mode='valid'),
+#     Activation('relu'),
+#     MaxPooling2D(pool_size=(10,1)),
+#     # Dropout(0.8),
+#     Convolution2D(32,5,1),
+#     Activation('relu'),
+#     MaxPooling2D(pool_size=(2,1)),
+#     # Dropout(0.8),
+#     Flatten(),
+#     Dense(60),
+#     # Dropout(0.8),
+#     Activation('relu'),
+#     Dense(3),
+#     Activation('softmax')
+# ]
+# [model.add(layer) for layer in layers]
 
 # print model.summary()
 
@@ -51,12 +101,13 @@ model.compile(loss='categorical_crossentropy',
               metrics=['accuracy']
               )
 # train
+
 nb_epoch = 100
-model.fit([X_dev],
+model.fit([X_dev]*len(n_wins),
           y_dev_onehot,
           batch_size=32,
           nb_epoch=nb_epoch,
-          validation_data=(X_test,y_test_onehot),
+          validation_data=([X_test]*len(n_wins),y_test_onehot),
           shuffle=True,
           verbose=1
           )
@@ -68,9 +119,9 @@ model.fit([X_dev],
 # save
 json_string = model.to_json()
 model_architecture = '/home/jdwang/PycharmProjects/weiboStanceDetection/cnn/model/' \
-                     'cnn_model_architecture_%depoch.json'%(nb_epoch)
+                     'cnn_model_architecture_%depoch_%dwin.json'%(nb_epoch,len(n_wins))
 open(model_architecture, 'w').write(json_string)
 model_weights = '/home/jdwang/PycharmProjects/weiboStanceDetection/cnn/model/' \
-                'cnn_model_weights_%depoch.h5'%(nb_epoch)
+                'cnn_model_weights_%depoch_%dwin.h5'%(nb_epoch,len(n_wins))
 model.save_weights(model_weights,overwrite=True)
 
