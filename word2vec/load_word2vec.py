e16872e10e513452ed1380a7a88ada2379cf9233
JDwang <383287471@qq.com>
2016-05-30 11:33:04 +0800

    update readme

--- word2vec/load_word2vec.py
+++ word2vec/load_word2vec.py
@@ -5,12 +5,14 @@ from gensim.models import Word2Vec
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
 
 # size = 50
+print '------'
 
 vector_file_path = '/home/jdwang/PycharmProjects/weiboStanceDetection/word2vec/model/' \
-                   'weibo_50size.gem'
+                   'weibodata_vectorB.gem'
 
 model = Word2Vec.load(vector_file_path)
-word = u'DIGITTAG'
+print '------'
+word = u'喜欢'
 print model.most_similar([word])
 print ','.join([item for item,_ in model.most_similar([word])])
 # print ','.join([item for item,_ in model.most_similar([word])])
