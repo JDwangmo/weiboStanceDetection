41b4d305cc1c99bd289d1874fd40941c63deb75d
JDwang <383287471@qq.com>
2016-06-10 22:03:30 +0800

    update reference

--- word2vec/load_word2vec.py
+++ word2vec/load_word2vec.py
@@ -8,11 +8,11 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 print '------'
 
 vector_file_path = '/home/jdwang/PycharmProjects/weiboStanceDetection/word2vec/model/' \
-                   'weibodata_vectorB.gem'
+                   'vector1000000.gem'
 
 model = Word2Vec.load(vector_file_path)
 print '------'
-word = u'喜欢'
+word = u'呃'
 print model.most_similar([word])
 print ','.join([item for item,_ in model.most_similar([word])])
 # print ','.join([item for item,_ in model.most_similar([word])])
