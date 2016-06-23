fd92b1e32889c0d75a716313008d99870d7b12c3
JDwang <383287471@qq.com>
2016-06-23 23:34:02 +0800

    update readme.md

---
+++ cnn/randEmbedding_cnn/randEmbedding_cnn_model.py
@@ -0,0 +1,30 @@
+#encoding=utf8
+__author__ = 'jdwang'
+__date__ = 'create date: 2016-06-23'
+__email__ = '383287471@qq.com'
+
+import numpy as np
+import pandas as pd
+import logging
+
+class RandEmbeddingCNN(object):
+    '''
+        一层CNN模型,随机初始化词向量,CNN-rand模型.
+    '''
+    def __init__(self,
+                 verbose=0):
+        # todo 初始化参数,filter size等
+        pass
+
+    def build_model(self):
+        # todo 构建模型
+        pass
+
+    def train(self):
+        # todo 模型
+        pass
+
+    def predict(self):
+        # todo 预测
+        pass
+
